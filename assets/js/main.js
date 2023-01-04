0edcaae0a079b009020634d2223b141c30fc175f
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2023-01-04 17:30:44 +0100

    colour change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
 
 
-document.getElementById('myButton').addEventListener('click', function() {
+document.getElementById('myButton').addEventListener('click', () => {
     if (document.body.style.backgroundColor === 'black') {
-    document.body.style.backgroundColor = 'white';
-    document.getElementById('myButton').innerHTML = '⚫️'
-    document.body.style.color = 'black';
+        document.body.style.color = 'black';
+        document.body.style.backgroundColor = 'white';
+        document.getElementById('myButton').innerHTML = '⚫️'
+        document.querySelector('.bg').style.backgroundColor = '#eeecf3'
+        document.querySelector('.article_five').style.borderTop = '2px solid black';
+        document.querySelector('.change_colour').style.color = 'black';
+        document.querySelector('.price').style.color = 'black';
+
+
     } else {
-    document.body.style.backgroundColor = 'black';
-    document.getElementById('myButton').innerHTML = '⚪️'
-    document.body.style.color = 'white';
+            document.body.style.color = 'white';
+            document.body.style.backgroundColor = 'black';
+            document.getElementById('myButton').innerHTML = '⚪️'
+            document.querySelector('.bg').style.backgroundColor = 'black'
+            document.querySelector('.article_five').style.borderTop = '2px solid white';
+            document.querySelector('.change_colour').style.color = 'white';
+            document.querySelector('.price').style.color = 'white';
     }
 })
 
 
-article_line
